4a79f157aef70e1e690485daac017770c6b4b8bf
Jean Franco Hernández <alu0101538853@ull.edu.es>
2025-05-08 14:20:21 +0000

    MongoDB Atlas modification

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,8 +1,16 @@
 // In this page we will create a server using express and connect to the database using mongoose
 import { connect } from "mongoose";
 
-connect("mongodb://localhost:27017/witcher-api").then(() => {
-  console.log("Connected to database");
-}).catch(err => {
-  console.error("Unable to connect to MongoDB server:", err);
-});
+const mongoURL = process.env.MONGODB_URL;
+
+if (!mongoURL) {
+  throw new Error("MONGODB_URL environment variable not set");
+}
+
+connect(mongoURL)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to MongoDB server:", err);
+  });
